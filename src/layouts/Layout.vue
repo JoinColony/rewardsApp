39d4f5abba9aeac424900108355cdaad1cb60a33
Tommy Cox <tommy.cox@protonmail.com>
2019-12-05 19:25:04 -0700

    Sidebar menu scaffolding

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -5,17 +5,47 @@
         <q-toolbar-title>
           Rewards Distribution
         </q-toolbar-title>
+        <q-btn
+          v-if="$route.path == '/rewards'"
+          flat
+          @click="drawerRight = !drawerRight"
+          round
+          dense
+          icon="menu"
+        />
       </q-toolbar>
     </q-header>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-drawer
+      v-if="$route.path == '/rewards'"
+      side="right"
+      v-model="drawerRight"
+      show-if-above
+      bordered
+      :width="200"
+      :breakpoint="500"
+      content-class="bg-grey-3"
+    >
+      <q-scroll-area class="fit">
+        <div class="q-pa-sm">
+          <div v-for="n in 50" :key="n">Drawer {{ n }} / 50</div>
+        </div>
+      </q-scroll-area>
+    </q-drawer>
   </q-layout>
 </template>
 
 <script>
 export default {
-  name: "Layout"
+  name: "Layout",
+  data() {
+    return {
+      drawerRight: false
+    };
+  }
 };
 </script>
