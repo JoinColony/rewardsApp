1d018df724a8e76160b5231faa59e8ec5c0f2748
Tommy Cox <tommy.cox@protonmail.com>
2019-12-05 18:59:42 -0700

    Clean up navigation guard

--- src/boot/navGuard.js
+++ src/boot/navGuard.js
@@ -1,8 +1,12 @@
 export default ({ router, store }) => {
+  // Make sure there's a wallet loaded or redirect to login.
   router.beforeEach(async (to, from, next) => {
-    if (store.getters["app/getWallet"] !== null || to.path == "/") {
+    // Ensure there's a wallet or that we're going to login.
+    if (store.getters["app/getWallet"] || to.path == "/") {
+      // Continue...
       next();
     } else {
+      // Otherwise redirect to login.
       next("/");
     }
   });
