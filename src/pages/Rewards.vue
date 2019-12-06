2491635667941c5428352cd8795ebbd1432f2909
Tommy Cox <tommy.cox@protonmail.com>
2019-12-05 19:13:54 -0700

    Rewards list scaffolding

--- src/pages/Rewards.vue
+++ src/pages/Rewards.vue
@@ -1,7 +1,92 @@
 <template>
-  <div>
+  <q-page class="flex flex-center column">
     <h3>Rewards</h3>
-  </div>
+    <q-list>
+      <q-item>
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">
+            Secondary line text. Lorem ipsum dolor sit amet, consectetur
+            adipiscit elit.
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+          <q-icon name="star" color="yellow" />
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>
+            Secondary line text. Lorem ipsum dolor sit amet, consectetur
+            adipiscit elit.
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>Voted!</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>
+            Secondary line text. Lorem ipsum dolor sit amet, consectetur
+            adipiscit elit.
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-badge color="teal" label="10k" />
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>
+            Secondary line text. Lorem ipsum dolor sit amet, consectetur
+            adipiscit elit.
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>2 min ago</q-item-label>
+          <div class="text-orange">
+            <q-icon name="star" />
+            <q-icon name="star" />
+            <q-icon name="star" />
+          </div>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>
+            Secondary line text. Lorem ipsum dolor sit amet, consectetur
+            adipiscit elit.
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>meta</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
 </template>
 
 <script>
