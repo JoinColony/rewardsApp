77d1914a458c7a3dedfd8cf679fe5a551b610da9
Tommy Cox <tommy.cox@protonmail.com>
2019-12-06 21:43:18 -0700

    Remove drawer in favor of sidebar component

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -7,47 +7,17 @@
             Colony Rewards Distribution
           </q-btn>
         </q-toolbar-title>
-        <q-btn
-          v-if="$route.path == '/rewards'"
-          flat
-          @click="drawerRight = !drawerRight"
-          round
-          dense
-          icon="menu"
-        />
       </q-toolbar>
     </q-header>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
-    <q-drawer
-      v-if="$route.path == '/rewards'"
-      side="right"
-      v-model="drawerRight"
-      show-if-above
-      bordered
-      :width="200"
-      :breakpoint="500"
-      content-class="bg-grey-3"
-    >
-      <q-scroll-area class="fit">
-        <div class="q-pa-sm">
-          <div v-for="n in 50" :key="n">Drawer {{ n }} / 50</div>
-        </div>
-      </q-scroll-area>
-    </q-drawer>
   </q-layout>
 </template>
 
 <script>
 export default {
-  name: "Layout",
-  data() {
-    return {
-      drawerRight: false
-    };
-  }
+  name: "Layout"
 };
 </script>
