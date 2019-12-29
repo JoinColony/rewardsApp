0db16bce2715bde7bfcd4ab46a03c7127d54b0dd
Tommy Cox <tommy.cox@protonmail.com>
2019-12-29 02:51:11 -0700

    Add some style for fun

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="lhh Lpr lFf">
+    <q-header class="transparent shadow-1">
       <q-toolbar>
         <q-toolbar-title>
           <q-btn to="/select-colony" no-caps flat>
@@ -21,3 +21,13 @@ export default {
   name: "Layout"
 };
 </script>
+
+<style lang="stylus" scoped>
+.q-header {
+  color: #000;
+}
+
+.q-page-container {
+  background: linear-gradient(0deg, rgba(146, 216, 220, 0.1) 0%, rgba(246, 185, 185, 0.1) 100%) #F4F6FA;
+}
+</style>
