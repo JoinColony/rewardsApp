fb82141d814d0d5609b3ded62b388d19ee1d0ac4
Tommy Cox <tommy.cox@protonmail.com>
2020-04-03 00:55:12 -0600

    There's only selectedPayout, never selectedToken

--- src/store/app/getters.js
+++ src/store/app/getters.js
@@ -1,11 +1,3 @@
-export function rewardPayoutInfo(state) {
-  return token => {
-    return state.rewardPayouts.find(
-      payoutInfo => payoutInfo.tokenAddress === token
-    );
-  };
-}
-
 export function payoutToken(state) {
   return payoutToken => {
     return state.rewardPotTokens.find(token => token.token === payoutToken);
